c44a937cbc80b8fa6d1ebdd7656442bcf159e522
Vladislav Lapidus <it.lapidus@gmail.com>
2019-12-02 22:59:28 +0200

    Init

---
+++ WebAPIEx/WebAPIEx/Models/AuthentificationContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+namespace WebAPIEx.Models
+{
+    public class AuthentificationContext : IdentityDbContext
+    {
+        public AuthentificationContext(DbContextOptions options) : base(options)
+        {
+
+        }
+    }
+}    
---
+++ WebAPIEx/WebAPIEx/Startup.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using WebAPIEx.Models;
+namespace WebAPIEx
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddDbContext<AuthentificationContext>(options =>
+            {
+                options.UseSqlServer("DevConnection");
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+        }
+    }
+}
