7661bf9d55702fefb87a1c9097c91aae990d7008
Vladislav Lapidus <it.lapidus@gmail.com>
2019-12-16 00:32:24 +0200

    User Registration & Components

--- Angular7/src/app/app-routing.module.ts
+++ Angular7/src/app/app-routing.module.ts
@@ -3,9 +3,8 @@ import { Routes, RouterModule } from '@angular/router';
 import { UserComponent } from './user/user.component';
 import { RegistrationComponent } from './user/registration/registration.component';
 
-
 const routes: Routes = [
-  {path : '', redirectTo: '/user/registration', pathMatch:'full'},
+  { path: '', redirectTo: '/user/registration', pathMatch: 'full' },
   {
     path: 'user', component: UserComponent,
     children: [
--- Angular7/src/app/user/user.component.ts
+++ Angular7/src/app/user/user.component.ts
@@ -11,5 +11,4 @@ export class UserComponent implements OnInit {
 
   ngOnInit() {
   }
-
 }
