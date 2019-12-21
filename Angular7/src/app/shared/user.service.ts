0bc80a6e40cdeb6f57a4eb089ff066a1928c3c65
Vladislav Lapidus <it.lapidus@gmail.com>
2019-12-21 19:07:53 +0200

    Pswd Validation

--- Angular7/src/app/shared/user.service.ts
+++ Angular7/src/app/shared/user.service.ts
@@ -1,20 +1,37 @@
 import { Injectable } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
+import { HttpClient } from "@angular/common/http";
+
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor(private fb: FormBuilder) { }
+  constructor(private fb: FormBuilder, private http: HttpClient) { }
+  readonly BaseURI = 'http://localhost:54277/api';
 
   formModel = this.fb.group({
     UserName: ['', Validators.required],
-    Email: [''],
+    Email: ['', Validators.email],
     FullName: [''],
     Passwords: this.fb.group({
-      Password: ['', Validators.required],
+      Password: ['', [Validators.required, Validators.minLength(4)]],
       ConfirmPassword: ['', Validators.required]
-    })
+    }, { validator: this.comparePasswords })
 
   });
+
+  comparePasswords(fb: FormGroup) {
+    let confirmPswrdCtrl = fb.get('ConfirmPassword');
+    // confirmPswrdCtrl.errors={passwordMismatch:true}
+    if (confirmPswrdCtrl.errors == null || "passwordMismatch" in confirmPswrdCtrl.errors) {
+      if (fb.get('Password').value != confirmPswrdCtrl.value)
+        confirmPswrdCtrl.setErrors({ passwordMismatch: true })
+      else
+        confirmPswrdCtrl.setErrors(null);
+
+    }
+  }
+
+
 }
