6902299624588922c543c04b247ae5a9cd00caab
Vladislav Lapidus <it.lapidus@gmail.com>
2019-12-21 17:18:15 +0200

    ngx-toastr & styles

--- Angular7/src/app/app.module.ts
+++ Angular7/src/app/app.module.ts
@@ -1,9 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserComponent } from './user/user.component';
 import { RegistrationComponent } from './user/registration/registration.component';
+import { UserService } from './shared/user.service';
 
 @NgModule({
   declarations: [
@@ -13,9 +18,15 @@ import { RegistrationComponent } from './user/registration/registration.componen
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot({
+      progressBar: true
+    })
   ],
-  providers: [],
+  providers: [UserService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
