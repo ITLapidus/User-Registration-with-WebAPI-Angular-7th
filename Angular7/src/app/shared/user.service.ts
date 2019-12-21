b47903110c762e0560c2b04db4a84b727f86f3d2
Vladislav Lapidus <it.lapidus@gmail.com>
2019-12-21 18:17:43 +0200

    user.service.ts & registration.component.html + service init

--- Angular7/src/app/shared/user.service.ts
+++ Angular7/src/app/shared/user.service.ts
@@ -1,8 +1,20 @@
 import { Injectable } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
+
+  formModel = this.fb.group({
+    UserName: ['', Validators.required],
+    Email: [''],
+    FullName: [''],
+    Passwords: this.fb.group({
+      Password: ['', Validators.required],
+      ConfirmPassword: ['', Validators.required]
+    })
+
+  });
 }
